bab6e3885ca368af76d5391a8f5794e86b284292
Nic O'Dell <nicodell88@gmail.com>
2019-12-16 10:07:06 +1100

    Added lab 2, removed *.pdf from global ignore

---
+++ Lab 2 - Actuators/Lab 2 - code template/DCcode/DCcode.ino
@@ -0,0 +1,23 @@
+/*
+ * MECH 2110 - Lab 2
+ * DC motor
+ */
+#define IN1 3
+#define IN2 4
+#define ENA 9
+void setup() {
+// put your setup code here, to run once:
+//TODO: initialise pins as output.
+    goForwards();
+}
+void loop() {
+// put your main code here, to run repeatedly:
+//TODO:
+//Gradually increase speed
+//Maintain speed
+//Gradually slow down
+}
+void goForwards(void)
+{
+//TODO: Set the pins IN1,IN2 appropriately.
+}
---
+++ Lab 2 - Actuators/Lab 2 - code template/ServoCode/ServoCode.ino
@@ -0,0 +1,39 @@
+#include <Servo.h>
+#define SERVOPIN 10
+Servo myservo; // create servo object to control a servo
+int pos = 0; // variable to store the servo position
+int dir = 0; // variable to store the servo direction
+void setup() {
+    Serial.begin(115200);
+    myservo.attach(SERVOPIN); // initialises the servo by attatching the servo on pin
+//...10 to the servo object
+}
+void loop() {
+//TODO: Change the range to sweep 0-90 deg
+    if(dir == 0) //if forwards
+    {
+        if(pos<180) //keep moving forwards
+        {
+            pos++;
+        }else //start moving backwards
+        {
+            dir = 1;
+            pos = 179;
+        }
+    }else //if backwards
+    {
+        if(pos>0) //keep moving backwards
+        {
+            pos--;
+        }else //start moving forwards
+        {
+            dir = 0;
+            pos = 1;
+        }
+    }
+//TODO: Send the updated position command to the servo
+    Serial.print("Position="); //print to the serial monitor
+    Serial.println(pos); //prints the position to the serial monitor + a new
+//...line
+    delay(15); // waits 15ms for the servo to reach the position
+}
